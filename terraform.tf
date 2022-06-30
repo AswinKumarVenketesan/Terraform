2c2214c0dba18a28ab5f009229085900ffdf6597
Aswin Kumar <51033679+AswinKumarVenketesan@users.noreply.github.com>
2022-06-30 15:58:42 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "IshaFoundation"
+    organization = "Aswin"
 
     workspaces {
-      name = "ishaservice-eks-ap-south-1"
+      name = "aswin-eks-ap-south-1"
     }
   }
 }
