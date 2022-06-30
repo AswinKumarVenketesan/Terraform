a9d706caaa945722a32d7529071ffd3098a58f46
Aswin Kumar <51033679+AswinKumarVenketesan@users.noreply.github.com>
2022-06-30 18:43:22 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -11,23 +11,23 @@ module "eks" {
     root_volume_type = "gp3"
   }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
-  ]
-}
+#   worker_groups = [
+#     {
+#       name                          = "worker-group-1"
+#       instance_type                 = "t2.small"
+#       additional_userdata           = "echo foo bar"
+#       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+#       asg_desired_capacity          = 2
+#     },
+#     {
+#       name                          = "worker-group-2"
+#       instance_type                 = "t2.medium"
+#       additional_userdata           = "echo foo bar"
+#       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+#       asg_desired_capacity          = 1
+#     },
+#   ]
+# }
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
