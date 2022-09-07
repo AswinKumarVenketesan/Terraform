d765a61be0f136c93844a208c54a5124fb058dee
Aswin Kumar <51033679+AswinKumarVenketesan@users.noreply.github.com>
2022-09-07 18:18:18 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ variable "region" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "sample-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
